3c3859456ff72c2fabe0ce37fe28fc9412fac509
XE <XE@XE1100>
2025-01-19 15:21:10 +0300

    Добавлены комментарии

--- Хокаге7(2)/Program.cs
+++ Хокаге7(2)/Program.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 
+// Типы данных в C#
+
 string variable_string = "Это строка";
 int variable_int = 12;
 double variable_double = 24.5;
 bool variable_bool = true;
 char variable_char = 'a';
 
+// Консольный вывод
+
 Console.WriteLine("Тип данных - строчка " + variable_string);
 Console.WriteLine("Тип данных - целочисленные " + variable_int);
 Console.WriteLine("Тип данных - дробный " + variable_double);
 Console.WriteLine("Тип данных - логические " + variable_bool);
 Console.WriteLine("Тип данных - символьные " + variable_char);
+
+// Сложение двух переменных в консоли для вывода 
+
+Console.WriteLine(variable_int + variable_double);
+
+// Консольный ввод и конвертация для него типов данных
 
 Console.WriteLine("Введите переменную типа int: ");
 int xInt;
